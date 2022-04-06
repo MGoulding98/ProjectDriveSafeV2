975c52b9354d13da3282a8e425b968bc009fdfd6
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-04-06 15:27:32 -0600

    added models

---
+++ Models/ViewModels/CrashesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ProjectDriveSafeV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectDriveSafeV2.Models.ViewModels
+{
+    public class CrashesViewModel
+    {
+        public IQueryable<Crash> Crashes { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
