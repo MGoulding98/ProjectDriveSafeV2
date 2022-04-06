627ebb50e430b92ad8adeef65310a5607861c41b
Gouldking <59857002+Gouldking@users.noreply.github.com>
2022-04-06 15:27:37 -0600

    Updating with files from V1

--- Startup.cs
+++ Startup.cs
@@ -27,13 +27,22 @@ namespace ProjectDriveSafeV2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RDSContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:CrashesDBConnection"]);
+            });
+
+            // Identity context files
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddScoped<ICollisionRepository, EFCollisionRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
