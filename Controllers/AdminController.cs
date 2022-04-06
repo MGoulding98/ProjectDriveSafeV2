4b5a3f844b7d41513e7c623b2c40daa1ae64a967
dpjensen22 <dpjensen22@gmail.com>
2022-04-06 15:27:35 -0600

    new proj

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjectDriveSafeV2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using ProjectDriveSafeV2.Models.ViewModels;
+
+namespace ProjectDriveSafeV2.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ICollisionRepository _repo { get; set; }
+
+        // constructor
+        public AdminController(ICollisionRepository temp)
+        {
+            _repo = temp;
+        }
+
+        [Authorize]
+        public IActionResult AdminView(string county, int pageNum = 1)
+        {
+            int pageSize = 50;
+
+            var x = new CrashesViewModel
+            {
+                Crashes = _repo.Crashes
+                .Where(x => x.COUNTY_NAME == county || county == null)
+                .OrderBy(c => c.CRASH_ID)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumCrashes = _repo.Crashes.Count(),
+                    CrashesPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            //IQueryable<Crash> blah = _repo.Crashes.Where(x => x.COUNTY_NAME == county || county == null);
+
+            return View(x);
+        }
+
+        [HttpGet]
+        public IActionResult CrashForm()
+        {
+            ViewBag.Crashes = _repo.Crashes.ToList();
+
+            return View(new Crash());
+        }
+
+        [HttpPost]
+        public IActionResult CrashForm(Crash c)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.CreateCollision(c);
+
+                return RedirectToAction("AdminView");
+            }
+            else
+            {
+                ViewBag.Crashes = _repo.Crashes.ToList();
+                return View(c);
+            }
+        }
+
+        // Edit Crash
+
+        [HttpGet]
+        public IActionResult EditCrash(int crashid)
+        {
+            ViewBag.Crashes = _repo.Crashes.ToList();
+
+            Crash c = _repo.GetCrash(crashid);
+            return View("CrashForm", c);
+        }
+
+        [HttpPost]
+        public IActionResult EditCrash(Crash c)
+        {
+            _repo.EditCollision(c);
+
+            return RedirectToAction("AdminView");
+        }
+
+
+        // Delete Crash
+        [HttpGet]
+        public IActionResult DeleteCrash(int crashid)
+        {
+            Crash c = _repo.GetCrash(crashid);
+            _repo.DeleteCollision(c);
+            return RedirectToAction("AdminView");
+        }
+    }
+}
+
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,37 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ProjectDriveSafeV2.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProjectDriveSafeV2.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjectDriveSafeV2.Models.ViewModels;
 
 namespace ProjectDriveSafeV2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private ICollisionRepository repo { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ICollisionRepository temp)
         {
-            _logger = logger;
+            repo = temp;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult ViewCrashes(int pageNum = 1)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            int pageSize = 50;
+
+            var x = new CrashesViewModel
+            {
+                Crashes = repo.Crashes
+                .OrderBy(c => c.CRASH_ID)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumCrashes = repo.Crashes.Count(),
+                    CrashesPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
         }
+
     }
 }
